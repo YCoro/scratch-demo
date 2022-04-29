339cd85333c94be1c16c0a265776f731990a12df
ycoro <cristhianmorales18@gmail.com>
2022-04-29 14:50:36 -0500

    initial files

---
+++ scratch.js
@@ -0,0 +1,7 @@
+ $("#card").wScratchPad({
+    size: 100, // The size of the brush/scratch.
+    bg:  './400.png', // Background (image path or hex color).
+    realtime    : true, 
+    fg: './overlay.png', // Foreground (image path or hex color).
+    'cursor': 'url("./coin1.png") 5 5, default',
+  });
