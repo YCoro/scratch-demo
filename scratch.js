c9b6fb839534fbe08bf380720c49cfd20748b855
ycoro <cristhianmorales18@gmail.com>
2022-05-02 16:06:39 -0500

    Added scratch sound. Added fireworks when revealed image at 50%

--- scratch.js
+++ scratch.js
@@ -1,7 +1,25 @@
+var audio = new Audio('./scratchh.wav');
+
+
  $("#card").wScratchPad({
     size: 100, // The size of the brush/scratch.
     bg:  './400.png', // Background (image path or hex color).
     realtime    : true, 
     fg: './overlay.png', // Foreground (image path or hex color).
+    scratchMove: function (e, percent) {
+      if ((percent > 50)) {
+        $('#fireworksDiv').removeClass('d-none');
+      }
+      if(percent < 99){
+        audio.play();
+        setTimeout(() => {
+          audio.pause();
+        }, 1000)
+      } else {
+        audio.pause();
+      }
+      
+    },
     'cursor': 'url("./coin1.png") 5 5, default',
+    
   });
