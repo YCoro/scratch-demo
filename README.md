538455dec8a8067a8883306474803fd4126c92d2
ycoro <cristhianmorales18@gmail.com>
2022-06-21 16:38:31 -0500

    Added new Scratch Images. Added new css and modified readme to run python Server

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 "# scratch-demo" 
+
+To run a python 3 server:
+`py -m http.server 8000`
+
+
+After server running type in browser:
+`http://localhost:8000/main.html`
--- scratch.js
+++ scratch.js
@@ -2,10 +2,10 @@ var audio = new Audio('./scratchh.wav');
 
 
  $("#card").wScratchPad({
-    size: 100, // The size of the brush/scratch.
-    bg:  './400.png', // Background (image path or hex color).
+    size: 75, // The size of the brush/scratch.
+    bg:  './scratchOn.png', // Background (image path or hex color).
     realtime    : true, 
-    fg: './overlay.png', // Foreground (image path or hex color).
+    fg: './scratchOff.png', // Foreground (image path or hex color).
     scratchMove: function (e, percent) {
       if ((percent > 50)) {
         $('#fireworksDiv').removeClass('d-none');
